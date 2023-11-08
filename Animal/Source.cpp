7b7ae0eab2051eddbabc7f1674d4f78fcac7bdba
hiskeel <borovetsmykola27@gmail.com>
2023-11-08 12:15:00 +0200

    animal

--- Animal/Source.cpp
+++ Animal/Source.cpp
@@ -25,12 +25,91 @@ public:
 
 class Bird : public Animal {
 private:
-	string movment;
+	string movement;
 	string voice;
+public:
+	Bird():movement("movement"), voice("voice"),Animal(){}
+	Bird(string t, float s, float w, string e, string movment, string voice):movement(movment), voice(voice), Animal(t,s,w,e){}
+	void Show() {
+		Animal::Show();
+		cout << "Movement: " << movement << "\nVoice: " << voice << endl;
+	}
+	void Voice() {
+		Animal::Voice();
+		cout << voice <<"ing" << endl;
+	}
+	void Move() {
+		Animal::Move();
+		cout << movement << endl;
+	}
+	
+
+};
+class Fish : public Animal {
+private:
+	string movement;
+	string voice;
+public:
+	Fish() :movement("movement"), voice("voice"), Animal() {}
+	Fish(string t, float s, float w, string e, string movment, string voice) :movement(movment), voice(voice), Animal(t, s, w, e) {}
+	void Show() {
+		Animal::Show();
+		cout << "Movement: " << movement << "\nVoice: " << voice << endl;
+	}
+	void Voice() {
+		Animal::Voice();
+		cout << voice << "ing" << endl;
+	}
+	void Move() {
+		Animal::Move();
+		cout << movement << endl;
+	}
+
+
+};
+class Reptile : public Animal {
+private:
+	string movement;
+	string voice;
+public:
+	Reptile() :movement("movement"), voice("voice"), Animal() {}
+	Reptile(string t, float s, float w, string e, string movment, string voice) :movement(movment), voice(voice), Animal(t, s, w, e) {}
+	void Show() {
+		Animal::Show();
+		cout << "Movement: " << movement << "\nVoice: " << voice << endl;
+	}
+	void Voice() {
+		Animal::Voice();
+		cout << voice << "ing" << endl;
+	}
+	void Move() {
+		Animal::Move();
+		cout << movement << endl;
+	}
+
 
 };
 
 void main() {
 
-
+	Bird gorobec("Sparrow", 20, 0.1, "Everywere", "Fly", "Cvirk");
+	gorobec.Show();
+	cout << "-----------------" << endl;
+	gorobec.Voice();
+	cout << "-----------------" << endl;
+	gorobec.Move();
+	cout << "\n\n==================" << endl;
+	Reptile rept("frog", 8, 0.2, "Swamp", "povz", "kvak");
+	rept.Show();
+	cout << "-----------------" << endl;
+	rept.Voice();
+	cout << "-----------------" << endl;
+	rept.Move();
+	cout << "\n\n==================" << endl;
+	Fish karac("Karac", 10, 0.3, "water", "swim", "splash");
+	karac.Show();
+	cout << "-----------------" << endl;
+	karac.Voice();
+	cout << "-----------------" << endl;
+	karac.Move();
 }
